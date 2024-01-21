61b58311ccd3cc0c671335b8db1a68ec19d586d9
Georg Dronov <dronovgeorg72@gmail.com>
2024-01-21 16:46:25 +0300

    Добавил версию через React.create, как работает понял

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import { restaurants } from "./constants/mock";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <div>
+  <div className="restaurants__wrapper">
     <div className="restaurants-01">
       <h2>{restaurants[0].name}</h2>
       <h3>Меню</h3>
@@ -144,14 +145,51 @@ root.render(
 // root.render(
 //   React.createElement("div", {
 //     children: [
-//       React.createElement("button", {
-//         children: React.createElement("span", { children: "Click me" }),
-//       }),
-//       React.createElement("button", {
-//         children: React.createElement("span", { children: "Click me" }),
-//       }),
-//       React.createElement("button", {
-//         children: React.createElement("span", { children: "Click me" }),
+//       React.createElement("div", {
+//         children: [
+//           React.createElement("h2", { children: `${restaurants[0].name}` }),
+//           React.createElement("h3", { children: "Меню" }),
+//           React.createElement("ul", {
+//             children: [
+//               React.createElement("li", {
+//                 children: React.createElement("p", {
+//                   children: `${restaurants[0].menu[0].name} ${restaurants[0].menu[0].price} рублей`,
+//                 }),
+//               }),
+//               React.createElement("li", {
+//                 children: React.createElement("p", {
+//                   children: `${restaurants[0].menu[1].name} ${restaurants[0].menu[1].price} рублей`,
+//                 }),
+//               }),
+//               React.createElement("li", {
+//                 children: React.createElement("p", {
+//                   children: `${restaurants[0].menu[2].name} ${restaurants[0].menu[2].price} рублей`,
+//                 }),
+//               }),
+//             ],
+//           }),
+//           React.createElement("h3", { children: "Отзывы" }),
+//           React.createElement("ul", {
+//             children: [
+//               React.createElement("li", {
+//                 children: React.createElement("p", {
+//                   children: `${restaurants[0].reviews[0].user} 
+//                   "${restaurants[0].reviews[0].text}"
+//                   ${restaurants[0].reviews[0].rating} / 5
+//                   `,
+//                 }),
+//               }),
+//               React.createElement("li", {
+//                 children: React.createElement("p", {
+//                   children: `${restaurants[0].reviews[1].user} 
+//                   "${restaurants[0].reviews[1].text}"
+//                   ${restaurants[0].reviews[1].rating} / 5
+//                   `,
+//                 }),
+//               }),
+//             ],
+//           }),
+//         ],
 //       }),
 //     ],
 //   })
