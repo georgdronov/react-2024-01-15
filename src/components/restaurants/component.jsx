94c7efaf4a7ee017d231c03bcc82d36ea85796b7
Georg Dronov <dronovgeorg72@gmail.com>
2024-01-27 12:04:04 +0300

    Работа над ошибками ДЗ

--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -1,23 +1,11 @@
+import { Restaurant } from "../restaurant/component";
+
 export const Restaurants = ({ restaurants }) => {
   return (
     <div>
-      <h3>{restaurants.name}</h3>
-      <h4>Меню</h4>
-      <ul>
-        {restaurants.menu.map((menuList) => (
-          <li>
-            {menuList.name} <b>{menuList.price} руб</b>
-          </li>
-        ))}
-      </ul>
-      <h4>Отзывы</h4>
-      <ul>
-        {restaurants.reviews.map((reviewsList) => (
-          <li>
-            {reviewsList.user}: {reviewsList.text}. <b>{reviewsList.rating} / 5</b>
-          </li>
-        ))}
-      </ul>
+      {restaurants.map((restaurant) => (
+        <Restaurant restaurant={restaurant} />
+      ))}
     </div>
   );
 };
